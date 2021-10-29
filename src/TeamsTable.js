ac9874e7a6e759fca9c1badcb97ed74c73900045
Jared Ramsey <ramsey_jared@bah.com>
2021-10-29 16:18:32 -0400

    Fixing warnings

--- src/TeamsTable.js
+++ src/TeamsTable.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
